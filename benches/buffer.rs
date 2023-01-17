0c1d87210d97c8a209a88614b6e36635973f7c0f
Emil Hofstetter <emil.k.hofstetter@gmail.com>
2023-01-17 13:19:03 -0500

    Add clone verion to fuzz

--- benches/buffer.rs
+++ benches/buffer.rs
@@ -64,24 +64,66 @@ fn push_pop_sling<const N: usize>(queue: RingBuffer<u8, N>, t: usize) {
     })
 }
 
+fn push_pop_sling_clone<const N: usize>(queue: RingBuffer<u8, N>, t: usize) {
+    let mut writer = queue.try_lock().unwrap();
+    let reader = queue.reader();
+
+    std::thread::scope(|s| {
+        for _ in 0..t {
+            s.spawn(|| loop {
+                let reader = reader.clone();
+                while let Some(_) = reader.pop_front() {}
+
+                let mut counter = 0;
+                while reader.pop_front().is_none() && counter < MAX_SPIN {
+                    counter += 1;
+                    core::hint::spin_loop();
+                }
+
+                if counter < MAX_SPIN {
+                    continue;
+                }
+
+                break;
+            });
+        }
+
+        for _ in 0..black_box(1000) {
+            writer.push_back(1);
+        }
+    })
+}
+
 fn benchmark(c: &mut Criterion) {
-    c.bench_function("push pop sling 4", |b| {
+    c.bench_function("sling 4", |b| {
         b.iter(|| push_pop_sling(RingBuffer::<u8, BUF_LEN>::new(), 4))
     });
 
-    c.bench_function("push pop sling 8", |b| {
+    c.bench_function("sling 8", |b| {
         b.iter(|| push_pop_sling(RingBuffer::<u8, BUF_LEN>::new(), 8))
     });
 
-    c.bench_function("push pop sling 16", |b| {
+    c.bench_function("sling 16", |b| {
         b.iter(|| push_pop_sling(RingBuffer::<u8, BUF_LEN>::new(), 16))
     });
 
-    c.bench_function("push pop lockfree 4", |b| b.iter(|| push_pop_lockfree(4)));
+    c.bench_function("sling clone 4", |b| {
+        b.iter(|| push_pop_sling_clone(RingBuffer::<u8, BUF_LEN>::new(), 4))
+    });
 
-    c.bench_function("push pop lockfree 8", |b| b.iter(|| push_pop_lockfree(8)));
+    c.bench_function("sling clone 8", |b| {
+        b.iter(|| push_pop_sling_clone(RingBuffer::<u8, BUF_LEN>::new(), 8))
+    });
 
-    c.bench_function("push pop lockfree 16", |b| b.iter(|| push_pop_lockfree(16)));
+    c.bench_function("sling clone 16", |b| {
+        b.iter(|| push_pop_sling_clone(RingBuffer::<u8, BUF_LEN>::new(), 16))
+    });
+
+    c.bench_function("lockfree 4", |b| b.iter(|| push_pop_lockfree(4)));
+
+    c.bench_function("lockfree 8", |b| b.iter(|| push_pop_lockfree(8)));
+
+    c.bench_function("lockfree 16", |b| b.iter(|| push_pop_lockfree(16)));
 }
 
 criterion_group!(benches, benchmark);
