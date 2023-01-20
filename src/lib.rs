52c3fa59eef3ea1c3dcf6a2eb38be2a26f88659f
Christian Eltzschig <me@elchris.org>
2023-01-20 09:52:22 +0100

    Some feedback

--- src/lib.rs
+++ src/lib.rs
@@ -130,6 +130,10 @@ impl<T: Copy, const N: usize> RingBuffer<T, N> {
         }
     }
 
+    /// Links to classes and methods can be written shorter:
+    ///  * A link to [`RingBuffer`]
+    ///  * A link to the method [`RingBuffer::try_lock()`]
+    ///
     /// Tries to acquire the [RingBuffer's](RingBuffer) [WriteGuard](WriteGuard). As there can
     /// only ever be one thread holding a [WriteGuard](WriteGuard), this fails if another thread is
     /// already holding the lock.
@@ -141,11 +145,22 @@ impl<T: Copy, const N: usize> RingBuffer<T, N> {
     /// ```
     #[inline]
     pub fn try_lock(&self) -> Result<WriteGuard<'_, T, N>, ()> {
-        if !self.locked.swap(true, Ordering::Acquire) {
-            Ok(WriteGuard { buffer: &self })
-        } else {
-            Err(())
+        // Here a compare_exchange loop is more fitting, only set the variable to true when the
+        // original value was false. In my opinion you also state the intent more clearly to
+        // other programmers what you actually would like to do here.
+        match self
+            .locked
+            .compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed)
+        {
+            Ok(_) => Ok(WriteGuard { buffer: self }),
+            Err(_) => Err(()),
         }
+
+        //if !self.locked.swap(true, Ordering::Acquire) {
+        //    Ok(WriteGuard { buffer: &self })
+        //} else {
+        //    Err(())
+        //}
     }
 
     /// Creates a new [ReadGuard](ReadGuard) which provides shared read access of the queue. The
@@ -159,6 +174,9 @@ impl<T: Copy, const N: usize> RingBuffer<T, N> {
     /// ```
     #[inline]
     pub fn reader(&self) -> ReadGuard<'_, T, N> {
+        // I do not understand why a ReadGuard is required? This is a single producer multi
+        // consumer queue in the end. If the number of consumers is not limited to some number I
+        // would provide the method pop_front directly in the RingBuffers interface
         ReadGuard {
             buffer: self,
             index: AtomicUsize::new(0),
@@ -176,8 +194,23 @@ impl<T: Copy, const N: usize> RingBuffer<T, N> {
         assert!(seq % 2 == 0);
 
         // Update the global version to be at newer than the current block version.
+        // In my opinion the memory order can be Relaxed here too.
+        //
+        // Release: synchronizes all writes happening before with a corresponding acquire
+        // Acquire: synchronizes with the corresponding release so that after the acquire the
+        //          memory can be read.
+        // With that being said I am unsure what memory you would like to sync here.
+        //
+        // When you write lock-free code you often have a piece of memory, in this case
+        // self.data which you sync over multiple threads with an atomic.
+        // Here I am unsure what the sync point (atomic is).
+        //
+        // In end write you sync everything in seq (Release), which makes sense and use it in
+        // pop_front with acquire so that you can read the data written in write.
         self.version.fetch_max(seq + 2, Ordering::Release);
 
+        // In my opinion not needed the sync point which syncs the data is in end_write
+        // with seq.fetch_add
         fence(Ordering::Release);
 
         index
@@ -187,6 +220,13 @@ impl<T: Copy, const N: usize> RingBuffer<T, N> {
     #[inline]
     fn end_write(&self, index: usize) {
         self.index.store((index + 1) % N, Ordering::Relaxed);
+
+        /// It makes sense to sync the memory after the write with release. When you create
+        /// the ReadGuard you acquire the memory and sync it with the other thread.
+        /// BUT (this is from the C++ memory model and rust is using the same model)
+        ///  * acquire without release is undefined behavior
+        ///  * release without acquire is undefined behavior
+        ///  * acquire - release MUST use the same atomic as synchronization point
         let seq = self.data[index].seq.fetch_add(1, Ordering::Release);
 
         // Ensure a consistent state.
@@ -210,6 +250,7 @@ impl<'read, T: Copy, const N: usize> Clone for ReadGuard<'read, T, N> {
     fn clone(&self) -> Self {
         ReadGuard {
             buffer: self.buffer,
+            // relaxed should suffice here.
             index: AtomicUsize::new(self.index.load(Ordering::Acquire)),
             version: AtomicUsize::new(self.version.load(Ordering::Relaxed)),
         }
@@ -229,6 +270,28 @@ impl<'read, T: Copy, const N: usize> ReadGuard<'read, T, N> {
             let seq1 = self.buffer.data[i].seq.load(Ordering::Acquire);
 
             if seq1 & 1 != 0 {
+                // From C++ Concurrency in Action - Anthony Williams
+                //
+                // Obstruction-Free: If all other threads are paused, then any given thread will complete its
+                //                     operation in a bounded number of steps.
+                // Lock-Free: If multiple threads are operating on a data structure, then after a bounded number
+                //             of steps one of them will complete its operation.
+                // Wait-Free: Every thread operating on a data structure will complete its operation in a bounded
+                //             number of steps, even if other threads are also operating on the data structure.
+                //
+                // Lock-Free guarantees that a misbehaving thread cannot block any other thread.
+
+                // This spin loop makes the queue non-lock-free. Let's assume I my algorithm has a
+                // bug and I write code like:
+                //
+                // if let Ok(mut writer) = buffer.try_lock() {
+                //     wait_until_the_end_of_time();
+                //     writer.push_back([12, 21, 04])
+                // };
+                //
+                // Then pop_front will wait forever in exactly this loop which is a deadlock
+                // so it is no lock-free
+
                 // Spin until we messages is written.
                 core::hint::spin_loop();
                 continue;
@@ -237,6 +300,14 @@ impl<'read, T: Copy, const N: usize> ReadGuard<'read, T, N> {
             // # Safety: We ensure validity of the read with the equality check later.
             let data: T = unsafe { read_volatile(self.buffer.data[i].message.get().cast()) };
 
+            // Why do we have to sync the memory again here? In seq1 = self.buffer.data[i]. it
+            // was already synced and now we have three options:
+            // 1. No push_back has happened and the data is still valid - no sync needed
+            // 2. In the middle of a push_back. The check where you read
+            //    self.buffer.data[i].seq.load(Ordering::Acquire) again will discover this and
+            //    the data is being discarded - no sync needed
+            // 3. New elemented has been pushed ... will be discovered again below - no sync
+            //    needed.
             fence(Ordering::Acquire);
 
             let seq2 = self.buffer.data[i].seq.load(Ordering::Acquire);
@@ -247,9 +318,18 @@ impl<'read, T: Copy, const N: usize> ReadGuard<'read, T, N> {
         }
     }
 
+    /// Do I assume correctly that the index is used to solve the ABA problem? If so I would
+    /// name it ABA counter to make the intent clear.
     /// Checks if we are reading data we have already consumed.
     #[inline]
     fn check_version(&self) -> Option<usize> {
+        // I do not understand what kind of data is synced here? I think the index can be always
+        // accessed with Relaxed.
+        //
+        // Another point could be the as-if rule. The compiler is allowed to reorder statements
+        // and Acquire/Release are forming some barriers but from a first look it seems it is
+        // not required here.
+
         // The current version of the
         let mut i = self.index.load(Ordering::Acquire);
 
